71553f1ff27dc4a900dad22ac86c647fbe4f9bf5
Ahmed Hatem <ahmadatimo2002@gmail.com>
2025-04-04 01:17:36 +0300

    backend updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.login import router as login_router
 
+
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
 
 @app.get("/")
 def read_root():
